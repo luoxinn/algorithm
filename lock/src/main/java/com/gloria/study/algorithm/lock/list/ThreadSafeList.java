0a74b570d672f0c41eae3df1a324ff65433ee6a7
luoxin <luoxingloria@163.com>
2020-04-22 22:13:44 +0800

    add ThreadSafeList

---
+++ lock/src/main/java/com/gloria/study/algorithm/lock/list/ThreadSafeList.java
@@ -0,0 +1,14 @@
+package com.gloria.study.algorithm.lock.list;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * @author luoxin
+ * @version 1.0
+ * @date 2020/4/20 11:18 下午
+ */
+public class ThreadSafeList {
+
+
+}
